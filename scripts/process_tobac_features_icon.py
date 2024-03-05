fbdf1d9e4fcfe9d4a4f99a89213c895b603bc3d1
William Jones <william.jones@physics.ox.ac.uk>
2024-03-05 23:30:44 +0100

    Convert precip to mm

--- scripts/process_tobac_features_icon.py
+++ scripts/process_tobac_features_icon.py
@@ -115,7 +115,7 @@ def main() -> None:
     )
 
     del bt
-    precip = dataset.pr.sel(time=slice(start_date, end_date-timedelta(minutes=1))).isel(cell=pix)
+    precip = dataset.pr.sel(time=slice(start_date, end_date-timedelta(minutes=1))).isel(cell=pix)/1e3
     
     # Get area array and calculate area of each segment
     segment_slice = segments[0]
