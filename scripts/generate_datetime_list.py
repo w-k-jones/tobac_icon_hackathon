c3d9c4e01a847b072b1a293199a833d5204bf09f
William Jones <william.jones@physics.ox.ac.uk>
2024-03-06 15:05:19 +0100

    Add datetime list script

---
+++ scripts/generate_datetime_list.py
@@ -0,0 +1,17 @@
+import sys
+import datetime
+
+if __name__ == '__main__':
+    t1_str = sys.argv[1]
+    t2_str = sys.argv[2]
+    hours = int(sys.argv[3])
+
+    fmt = '%Y-%m-%d-%H:%M:%S'
+ 
+    t1 = datetime.datetime.strptime( t1_str, fmt )
+    t2 = datetime.datetime.strptime( t2_str, fmt )
+ 
+    t = t1
+    while t <= t2:
+        print('%s' % t.strftime(fmt))
+        t += datetime.timedelta(hours=hours)
