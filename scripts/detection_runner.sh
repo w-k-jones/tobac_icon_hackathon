e64739f699df9aee58d5836929b5ecb99a8a267f
William Jones <william.jones@physics.ox.ac.uk>
2024-03-06 15:21:29 +0100

    Add default 1 hour

--- scripts/detection_runner.sh
+++ scripts/detection_runner.sh
@@ -17,4 +17,4 @@ ulimit -c 0
 export PATH=/work/bb1153/b380352/tools:$PATH
 cmd=./process_tobac_features_icon.py
 lfile=$*
-start_proc_from_list -n 128 -p $cmd $lfile 1
+start_proc_from_list -n 128 -p $cmd $lfile
